77bba1fae00e9e0d8b1c4255cc77ac8e8caffbe4
Jonghee Son <zzongaone@gmail.com>
2024-05-15 22:10:00 +0900

    Removed left wheel offset calculation, global odom reverted to slam_toolbox(scan matching)

--- src/ekf_odom_pub.cpp
+++ src/ekf_odom_pub.cpp
@@ -36,7 +36,7 @@ double distanceRight = 0;
 bool initialPoseRecieved = true;
 
 // Correction factor for misaligned wheels
-const double LEFTWHEEL_OFFSET = 0.03;					   // Offset of left wheel in meters (positive is to the forward)
+const double LEFTWHEEL_OFFSET = 0.0;					   // Offset of left wheel in meters (positive is to the forward)
 
 using namespace std;
 
@@ -123,8 +123,8 @@ void update_odom()
 	double cycleDistance = (distanceRight + distanceLeft) / 2;
 
 	// Calculate the number of radians the robot has turned since the last cycle
-	//double cycleAngle = asin((distanceRight - distanceLeft) / WHEEL_BASE);
-	double cycleAngle = atan(LEFTWHEEL_OFFSET / WHEEL_BASE) - atan((LEFTWHEEL_OFFSET + distanceLeft - distanceRight) / WHEEL_BASE);
+	double cycleAngle = asin((distanceRight - distanceLeft) / WHEEL_BASE);
+	//double cycleAngle = atan(LEFTWHEEL_OFFSET / WHEEL_BASE) - atan((LEFTWHEEL_OFFSET + distanceLeft - distanceRight) / WHEEL_BASE);
 
 	// Average angle during the last cycle
 	double avgAngle = cycleAngle / 2 + odomOld.pose.pose.orientation.z;
